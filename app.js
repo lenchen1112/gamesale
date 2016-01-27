4703de75fa2d8de7d1a552ea8199276fe0127b7d
Jason Huang <cjhuang@yahoo-inc.com>
2016-01-27 18:18:04 +0800

    fetch gamesale index pages

---
+++ app.js
@@ -0,0 +1,39 @@
+import request from 'superagent'
+import cheerio from 'cheerio'
+import mongoose, { Schema } from 'mongoose'
+import findOrCreate from 'mongoose-findorcreate'
+
+
+// set up MongoDB
+mongoose.connect('mongodb://localhost/gamesale')
+const SaleEntrySchema = new Schema({
+  url: String,
+  title: String
+})
+SaleEntrySchema.plugin(findOrCreate)
+const SaleEntry = mongoose.model('SaleEntry', SaleEntrySchema)
+
+
+// fetch pages
+const GAMESALE_INDEX_PAGE = 'https://www.ptt.cc/bbs/Gamesale/index.html'
+
+const parsePage = (pageUrl) => {
+  request
+    .get(pageUrl)
+    .end((err, res) => {
+      const $ = cheerio.load(res.text)
+
+      $('.r-ent .title').each((i, elem) => {
+        SaleEntry.findOrCreate({
+          url: $(elem).find('a').attr('href')
+        },{
+          title: $(elem).text().trim()
+        }, () => {});
+      });
+    })
+}
+
+parsePage(GAMESALE_INDEX_PAGE)
+
+// tear down MongoDB
+mongoose.disconnect()
