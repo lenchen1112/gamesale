6443c2fc34913f51f94f83734dd5309bdcf94b16
Jason Huang <cjhuang@yahoo-inc.com>
2016-01-28 14:46:13 +0800

    use Rx to fetch post periodically

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
 import request from 'superagent'
 import cheerio from 'cheerio'
 import notifier from 'node-notifier'
+import Rx from 'rx'
+import Promise from 'bluebird'
 
 // fetch pages
 const GAMESALE_INDEX_PAGE = 'https://www.ptt.cc/bbs/Gamesale/index.html'
 const KEY_PATTERN = /PS4.*售.*巫師3/
 
-const containsKeyword = (title, pattern) => {
-  return pattern.test(title)
+const notify = (entryTitle, entryUrl) => {
+  notifier.notify({
+    title: '有人要賣 巫師3 啦',
+    message: entryTitle,
+    open: entryUrl
+  })
 }
 
-const findEntryWithPattern = (pageUrl, keyPattern) => {
-  request
-    .get(pageUrl)
-    .end((err, res) => {
-      const $ = cheerio.load(res.text)
-
-      cheerio.load(res.text)('.r-ent .title').each((i, elem) => {
-        const entryTitle = $(elem).text().trim()
-        const entryUrl = 'https://www.ptt.cc' + $(elem).find('a').attr('href')
-
-        if (containsKeyword(entryTitle, keyPattern)) {
-          notifier.notify({
-            title: '有人要賣 巫師3 啦',
-            message: entryTitle,
-            open: entryUrl
-          })
+const findEntryWithPatternAsync = (pageUrl, keyPattern) => {
+  return new Promise((resolve, reject) => {
+    request
+      .get(pageUrl)
+      .end((err, res) => {
+        if (err) {
+          reject(err)
         }
+
+        const $ = cheerio.load(res.text)
+        let entries = []
+
+        cheerio.load(res.text)('.r-ent .title').each((i, elem) => {
+          const entryTitle = $(elem).text().trim()
+          const entryUrl = 'https://www.ptt.cc' + $(elem).find('a').attr('href')
+
+          if (keyPattern.test(entryTitle)) {
+            entries.push({
+              title: entryTitle,
+              url: entryUrl
+            })
+          }
+        })
+
+        resolve(entries)
       })
-    })
+  })
 }
 
-findEntryWithPattern(GAMESALE_INDEX_PAGE, KEY_PATTERN)
+Rx.Observable
+  .timer(0, 10000)
+  .flatMap(() => {
+    return Rx.Observable
+             .fromPromise(findEntryWithPatternAsync(GAMESALE_INDEX_PAGE, KEY_PATTERN))
+             .flatMap(x => {
+               return Rx.Observable.fromArray(x)
+             })
+  })
+  .distinctUntilChanged()
+  .subscribe((x) => {
+    notify(x.title, x.url)
+  })
