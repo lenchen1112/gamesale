7ca96948adb70a495bd132136213b155c81eec99
Jason Huang <cjhuang@yahoo-inc.com>
2016-02-16 14:17:12 +0800

    use eslint-config-reactjs

--- app.js
+++ app.js
@@ -1,67 +1,67 @@
-import request from 'superagent'
-import cheerio from 'cheerio'
-import notifier from 'node-notifier'
-import Rx from 'rx'
-import Promise from 'bluebird'
-import { Spinner } from 'cli-spinner'
-import Debug from 'debug'
+import request from 'superagent';
+import cheerio from 'cheerio';
+import notifier from 'node-notifier';
+import Rx from 'rx';
+import Promise from 'bluebird';
+import { Spinner } from 'cli-spinner';
+import Debug from 'debug';
 
-const spinner = new Spinner('processing.. %s')
-spinner.setSpinnerString('|/-\\')
-spinner.start()
+const spinner = new Spinner('processing.. %s');
+spinner.setSpinnerString('|/-\\');
+spinner.start();
 
-const debug = new Debug('gamesale')
+const debug = new Debug('gamesale');
 
-// fetch pages
-const GAMESALE_INDEX_PAGE = 'https://www.ptt.cc/bbs/Gamesale/index.html'
+// constants
+const GAMESALE_INDEX_PAGE = 'https://www.ptt.cc/bbs/Gamesale/index.html';
 const PATTERNS = [
   /PS4.*售.*(人中之龍|人龍).*(0|零)/i,
   /PSV.*售.*魔龍/i
-]
-const FETCH_PERIOD = 20 * 1000
+];
+const FETCH_PERIOD = 20 * 1000;
 
-
+// functions
 const fetchEntries = pageUrl => {
   return new Promise((resolve, reject) => {
     request
       .get(pageUrl)
       .end((err, res) => {
         if (err) {
-          reject(err)
+          reject(err);
         }
 
-        const $ = cheerio.load(res.text)
+        const $ = cheerio.load(res.text);
         const entries = $('.r-ent .title').map((i, elem) => {
-          const title = $(elem).text().trim()
-          const url = 'https://www.ptt.cc' + $(elem).find('a').attr('href')
+          const title = $(elem).text().trim();
+          const url = 'https://www.ptt.cc' + $(elem).find('a').attr('href');
 
           return {
             title,
             url
-          }
-        })
+          };
+        });
 
-        resolve(entries)
-      })
-  })
-}
+        resolve(entries);
+      });
+  });
+};
 
 const filterWithPatterns = patterns => entry => {
   return patterns.map(pattern => pattern.test(entry.title))
-                 .reduce((a, b) => a || b, false)
-}
+                 .reduce((a, b) => a || b, false);
+};
 
 const sendNotification = entry => {
-  debug('')
-  debug(entry)
-  debug(new Date())
+  debug('');
+  debug(entry);
+  debug(new Date());
 
   notifier.notify({
     title: '徵到啦',
     message: entry.title,
     open: entry.url
-  })
-}
+  });
+};
 
 Rx.Observable
   .timer(0, FETCH_PERIOD)
@@ -69,4 +69,4 @@ Rx.Observable
   .flatMap(Rx.Observable.fromArray)
   .filter(filterWithPatterns(PATTERNS))
   .distinct()
-  .subscribe(sendNotification)
+  .subscribe(sendNotification);
